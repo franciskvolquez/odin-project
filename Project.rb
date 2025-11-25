33307f58a01e4bd94f449a6cf5d6a33aa0d89707
FranciskV <franciskvolquez@hotmail.com>
2025-11-25 10:14:00 -0400

    -Definition of method substring

---
+++ Project.rb
@@ -0,0 +1,16 @@
+#dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substrings (word = "", dictionary = [""])
+
+  hash = dictionary.to_h{|element| [element,0]}
+  downcased_word = word.downcase
+
+  hash.each do |k,v|
+    hash[k]+= + downcased_word.scan(k.downcase).count
+  end
+
+  puts  hash.select{|k,v| v >0 }
+end
+
+#substrings("below", dictionary)
+#substrings("Howdy partner, sit down! How's it going?", dictionary)
